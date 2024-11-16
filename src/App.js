331cf9a64a9e86969936f6347241a7d9c3df76ae
anees635 <don3304581027@gmail.com>
2024-11-16 17:41:22 +0500

    New

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-// import Alert from './Alert'
-// import Navbar from './Navbar.js';
+import Alert from './Alert'
+import Navbar from './Navbar.js';
 // import About from './About.js';
 import Textform from './Textform.js';
-import React from 'react'
+import React,{useState}from 'react'
 // import {
 //   BrowserRouter,
 //   Routes,
@@ -15,54 +15,59 @@ import React from 'react'
 
 function App() {
   
-//   const [btntext, setbtntext] = useState('Enable Dark Mode');
-//   const [border, setborder]= useState({border : '1px solid black'});
+  const [btntext, setbtntext] = useState('Enable Dark Mode');
+  const [border, setborder]= useState({border : '1px solid black'});
 
-//   let webBody = document.body;
+  let webBody = document.body;
   
 
-//   const [style, setStyle] =useState({
-//     color: 'black',
-//     backgroundColor: 'white'
-//    });
+  const [style, setStyle] =useState({
+    color: 'black',
+    backgroundColor: 'white'
+   });
    
-//   const [mode, setmode]= useState("");
+  const [mode, setmode]= useState("");
  
-//  const toogleStyle =() =>{
-//   if(style.color === 'black')
-//   { setmode("dark");
-//       setStyle({color: 'white', backgroundColor: 'rgb(36, 35, 35)'});
-//       setbtntext('Disable Dark Mode');
-//       setborder({border: '1px solid white'});
-//       webBody.style.color = 'white';
-//       webBody.style.backgroundColor = 'rgb(36, 35, 35)';
-//       makealert("Dark Mode is activated", "success");
+ const toogleStyle =() =>{
+  if(style.color === 'black')
+  { setmode("dark");
+      setStyle({color: 'white', backgroundColor: 'rgb(36, 35, 35)'});
+      setbtntext('Disable Dark Mode');
+      setborder({border: '1px solid white'});
+      webBody.style.color = 'white';
+      webBody.style.backgroundColor = 'rgb(36, 35, 35)';
+      makealert("Dark Mode is activated", "success");
       
-//   }
-//   else{
-//     setmode("light");
-//     setStyle({color: 'black', backgroundColor: 'white'});
-//     setbtntext('Enable Dark Mode');
-//     setborder({border: '1px solid black'});
-//     webBody.style.color = 'black';
-//     webBody.style.backgroundColor = 'white';
-//     makealert("Light Mode is activated", "success");
+  }
+  else{
+    setmode("light");
+    setStyle({color: 'black', backgroundColor: 'white'});
+    setbtntext('Enable Dark Mode');
+    setborder({border: '1px solid black'});
+    webBody.style.color = 'black';
+    webBody.style.backgroundColor = 'white';
+    makealert("Light Mode is activated", "success");
 
-//   }
-//  };
+  }
+ };
   
-//   const [alert, setalert] = useState(null);
+  const [alert, setalert] = useState(null);
   
-//   const makealert = (message, type)=>{
-//     setalert({
-//       mes: message,
-//       type: type
-//     })
-//     setTimeout(() => {
-//       setalert(null);
-//     }, 1200);
-//   }
+  const makealert = (message, type)=>{
+    setalert({
+      mes: message,
+      type: type
+    })
+    setTimeout(() => {
+      setalert(null);
+    }, 1200);
+  }
   return (
+    <>
+    <Alert alert={alert}/>
+    <Navbar title="TextTracker" mode={mode}/>
+    <Textform heading="Enter text below to analyze" makealert={makealert} mode={style} btntext={btntext} border={border} func={toogleStyle}/>
+    </>
 
   //   <BrowserRouter>
   //   <Alert alert={alert}/>
@@ -75,7 +80,7 @@ function App() {
   //     </Route>
   //   </Routes>
   // </BrowserRouter>
-     <Textform/>
+     
   );
  
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet,Link } from 'react-router-dom'
+// import { Outlet,Link } from 'react-router-dom'
 import './Navbar.css';
 import PropTypes from 'prop-types'
 export default function Navbar(props) {
@@ -8,7 +8,7 @@ export default function Navbar(props) {
      <nav className={`navbar navbar-expand-lg navbar-${props.mode} ${props.mode} flexcontainer`}>
   <div className="container-fluid">
     <div>
-      <Link className="navbar-brand" to="/">{props.title}</Link>
+      <a className="navbar-brand" href="/">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
          <span className="navbar-toggler-icon"></span>
       </button>
@@ -16,10 +16,10 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse " id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/Text">Home</Link>
+          <a className="nav-link active" aria-current="page" href="/Text">Home</a>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/About">About</Link>
+          <a className="nav-link" href="/About">About</a>
         </li>
         
        
@@ -29,7 +29,7 @@ export default function Navbar(props) {
  </nav>
         
 
-        <Outlet/>
+        {/* <Outlet/> */}
    </>
   )
 }
